c3b84346a472e48908e4de7fb46ed87288dc62ab
2004Island <varun@firecrest.org>
2023-08-04 20:57:14 -0700

    STL files

--- code/code.ino
+++ code/code.ino
@@ -2,13 +2,14 @@
 #include <SPI.h>
 #include "graphics.h"
 
-#define TFT_BLUE 0x757F
+#define TFT_BLUE 0x079F
 #define TFT_YELLOWGREEN 0xC749
 
 TFT_eSPI tft = TFT_eSPI();
 
 const int alcPin = A0;
 int sensorValue = 0;
+int lastBitmap = 0;
 
 void setup() {
   // TFT setup
@@ -38,33 +39,46 @@ void loop() {
  
   if (sensorValue < 170) {
     drawScreen(0, TFT_BLUE);
+    lastBitmap = 0;
 
   } else if (sensorValue >= 170 && sensorValue < 340) {
     drawScreen(1, TFT_GREEN);
+    lastBitmap = 1;
 
   } else if (sensorValue >= 340 && sensorValue < 510) {
     drawScreen(2, TFT_YELLOWGREEN);
+    lastBitmap = 2;
 
   } else if (sensorValue >= 510 && sensorValue < 680) {
     drawScreen(3, TFT_YELLOW);
+    lastBitmap = 3;
 
   } else if (sensorValue >= 680 && sensorValue < 850) {
     drawScreen(4, TFT_ORANGE);
+    lastBitmap = 4;
 
   } else {
     drawScreen(5, TFT_RED);
+    lastBitmap = 5;
   }
 
 }
 
 void drawScreen(int type, uint16_t color) {
-  tft.drawBitmap(75, -10, epd_bitmap_allArray[type], 100, 100, color);
+  if (type != lastBitmap) {
+    tft.fillRect(75, -10, 100, 100, TFT_BLACK);
+    tft.drawBitmap(75, -10, epd_bitmap_allArray[type], 100, 100, color);
+  }
+  else {
+    tft.drawBitmap(75, -10, epd_bitmap_allArray[type], 100, 100, color);
+  }
+
   tft.fillRectHGradient(11, 58, 60, 10, TFT_GREEN, TFT_RED);
   tft.drawRect(10, 57, 62, 12, TFT_WHITE);
   tft.fillRect(10 + (sensorValue/18), 55, 5, 16, TFT_WHITE);
   tft.drawCentreString(String(sensorValue), 40, 35, 1);
   delay(500);
   tft.fillRect(10, 34, 80, 20, TFT_BLACK);
-  tft.fillRect(75, -10, 100, 100, TFT_BLACK);
-  tft.fillRect(10, 55, 80, 20, TFT_BLACK);
+  tft.fillRect(10 + (sensorValue/18), 55, 5, 16, TFT_BLACK);
+  tft.fillRectHGradient(11, 58, 60, 10, TFT_GREEN, TFT_RED);
 }
